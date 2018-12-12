2f351c3c96c374070e2bdf2eace6368df3ae69a3
Tobias Petrich <tobias.petrich@campus.tu-berlin.de>
2018-12-12 18:47:35 +0100

    Add Atom config

---
+++ atom/.atom/packages.zsh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+apm install atom-keyboard-macros
+apm install language-latex
+apm install language-matlab
+apm install latex
+apm install latex-autocomplete
+apm install platformio-ide-terminal
