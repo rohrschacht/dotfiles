17543f445aa25c451cdfe8a30b813aef49c79119
Tobias Petrich <tobias.petrich@campus.tu-berlin.de>
2019-04-29 19:01:09 +0200

    Make fish fit for emacs

--- myfish/.config/fish/config.fish
+++ myfish/.config/fish/config.fish
@@ -1,2 +1,15 @@
 set -x PATH $PATH $HOME/.local/bin $HOME/go/bin
 set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+
+# Configuration for fish shell in emacs
+if test -n "$INSIDE_EMACS"
+	set -x TERM eterm-color
+end
+
+function fish_title
+	if test -n "$INSIDE_EMACS"
+		true
+	else
+		echo (status current-command) (__fish_pwd)
+	end
+end
