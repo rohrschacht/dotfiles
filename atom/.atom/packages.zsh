21774820f3f5a2794d599c6de13ac203ae5f5fa9
Tobias Petrich <tobias.petrich@campus.tu-berlin.de>
2020-05-31 21:40:33 +0200

    Delete atom config

--- atom/.atom/packages.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-apm install atom-keyboard-macros
-apm install language-latex
-apm install language-matlab
-apm install latex
-apm install latex-autocomplete
-apm install platformio-ide-terminal
