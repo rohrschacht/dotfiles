3382df09883fa2094c9133f44c1d0399a572c323
Tobias Petrich <tobias.petrich@campus.tu-berlin.de>
2020-05-31 22:00:31 +0200

    Update Plasma wm.sh to current

--- plasma/.config/plasma-workspace/env/wm.sh
+++ plasma/.config/plasma-workspace/env/wm.sh
@@ -1,8 +1,4 @@
-# Disable KWin and use i3gaps as WM
-export KDEWM=/usr/bin/i3
-
-# Compositor (Animations, Shadows, Transparency)
-# xcompmgr -C
-
-# Start compton as daemon (-b)
-compton -b --config ~/.config/compton.conf
+#!/bin/sh
+# export KDEWM=/usr/bin/bspwm
+export KDEWM=/usr/local/bin/dwm
+# export KDEWM=/usr/bin/i3
