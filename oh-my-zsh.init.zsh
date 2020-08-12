2843c110a0220e78552729f0a75b167dcb4506dd
Tobias Petrich <tobias.petrich@campus.tu-berlin.de>
2020-08-13 00:03:30 +0200

    Fix oh-my-zsh directory location in oh-my-zsh.init.zsh

--- oh-my-zsh.init.zsh
+++ oh-my-zsh.init.zsh
@@ -7,7 +7,7 @@ rm .zshrc
 echo export ZDOTDIR="$HOME/.config/zsh" | sudo tee /etc/zsh/zshenv
 popd
 stow oh-my-zsh
-cd ~/.config/.oh-my-zsh/plugins
+cd ~/.config/oh-my-zsh/plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions.git
 git clone https://github.com/zsh-users/zsh-history-substring-search.git
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
