72ca5b1319ba4bf2b77e426ec663a396b4b4c792
Tobias Petrich <tobias.petrich@campus.tu-berlin.de>
2020-08-12 23:53:20 +0200

    Update oh-my-zsh.init.zsh

--- oh-my-zsh.init.zsh
+++ oh-my-zsh.init.zsh
@@ -2,10 +2,12 @@
 
 pushd ~
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+mv .oh-my-zsh .config/
 rm .zshrc
+echo export ZDOTDIR="$HOME/.config/zsh" > /etc/zsh/zprofile
 popd
 stow oh-my-zsh
-cd ~/.oh-my-zsh/plugins
+cd ~/.config/.oh-my-zsh/plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions.git
 git clone https://github.com/zsh-users/zsh-history-substring-search.git
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
