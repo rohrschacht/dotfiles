8f82912fa4acf466961da00a5b030c82077914f3
Tobias Petrich <tobias.petrich@campus.tu-berlin.de>
2020-08-13 01:06:30 +0200

    Remove pushd/popd dependency from oh-my-zsh.init.zsh

--- oh-my-zsh.init.zsh
+++ oh-my-zsh.init.zsh
@@ -1,11 +1,12 @@
 #!/usr/bin/zsh
 
-pushd ~
+PWDSAVE=$PWD
+cd $HOME
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 mv .oh-my-zsh .config/oh-my-zsh
 rm .zshrc
 echo export ZDOTDIR="$HOME/.config/zsh" | sudo tee /etc/zsh/zshenv
-popd
+cd $PWDSAVE
 stow oh-my-zsh
 cd ~/.config/oh-my-zsh/plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions.git
