90414f6e59ec7c80f264c6d8dbb939830c36d8fe
Tobias Petrich <tobias.petrich@campus.tu-berlin.de>
2021-02-20 23:19:13 +0100

    oh-my-zsh.init.zsh cleanup git clones

--- oh-my-zsh.init.zsh
+++ oh-my-zsh.init.zsh
@@ -8,8 +8,7 @@ rm .zshrc
 echo export ZDOTDIR="$HOME/.config/zsh" | sudo tee /etc/zsh/zshenv
 cd $PWDSAVE
 stow oh-my-zsh
-cd ~/.config/oh-my-zsh/plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions.git
-git clone https://github.com/zsh-users/zsh-history-substring-search.git
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.config/oh-my-zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search.git ~/.config/oh-my-zsh/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/oh-my-zsh/plugins/zsh-syntax-highlighting
 git clone https://github.com/romkatv/powerlevel10k.git ~/.config/oh-my-zsh/themes/powerlevel10k
