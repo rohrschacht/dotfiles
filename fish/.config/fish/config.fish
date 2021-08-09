b48447e8db1a09a33da382891fa2a4f24a16dc82
Tobias Petrich <tobias.petrich@campus.tu-berlin.de>
2021-08-10 00:47:29 +0200

    fish: add detection for exa alias

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,3 +6,10 @@ if test -n "$INSIDE_EMACS"
 	set -x TERM eterm-color
 end
 
+if command -v exa >/dev/null 2>&1
+	alias ll 'exa -lg'
+	alias la 'exa -lga'
+else
+	alias ll 'ls -lh'
+	alias la 'ls -lah'
+end
