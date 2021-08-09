efa0d456137736991cad2a5ddf574b1ccf37cf22
Tobias Petrich <tobias.petrich@campus.tu-berlin.de>
2021-08-10 01:08:00 +0200

    fish: add ssh bootstrapping function

---
+++ fish/.config/fish/functions/ssh.fish
@@ -0,0 +1,16 @@
+# Defined in /tmp/fish.li7EaM/ssh.fish @ line 2
+function ssh
+	command ssh $argv 'which fish >/dev/null 2>&1 && which git >/dev/null 2>&1 && which stow >/dev/null 2>&1 || exit 1 \
+		&& [ -d "$HOME/.dotfiles" ] && { cd "$HOME/.dotfiles" && git pull && [ -L "$HOME/.config/fish" ] || { rm -rf "$HOME/.config/fish" && cd "$HOME/.dotfiles" && stow fish oh-my-fish && [ -d "$HOME/.local/share/omf" ] || fish -c oh-my-fish-init ; }; } \
+		|| { git clone https://github.com/Rohrschacht/dotfiles.git "$HOME/.dotfiles" && cd "$HOME/.dotfiles" && stow fish oh-my-fish vim \
+		 && fish -c oh-my-fish-init; }'
+
+	if not test $status -eq 0
+		echo ssh-bootstrap-config: Host does not have fish, git and stow installed or they are not in path
+		command ssh -t $argv
+		exit
+	end
+
+	# open interactive fish with my config
+	command ssh -t $argv TERM=xterm-256color fish
+end
