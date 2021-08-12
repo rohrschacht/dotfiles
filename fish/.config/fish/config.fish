7b975c97a07666a2234bbd2bb4b99ee12bf4940d
Tobias Petrich <tobias.petrich@campus.tu-berlin.de>
2021-08-12 14:23:33 +0200

    fish: add XDG_ variables and VIMINIT

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,12 @@
 set -x PATH $PATH $HOME/.local/bin $HOME/go/bin
 set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 
+set -x XDG_CONFIG_HOME "$HOME/.config"
+set -x XDG_CACHE_HOME "$HOME/.cache"
+set -x XDG_DATA_HOME "$HOME/.local/share"
+
+set -x VIMINIT "source $XDG_CONFIG_HOME/vim/.vimrc"
+
 # Configuration for fish shell in emacs
 if test -n "$INSIDE_EMACS"
 	set -x TERM eterm-color
