41c111553695173c3d509318856ff66dfa5c7012
Tobias Petrich <tobias.petrich@campus.tu-berlin.de>
2021-08-26 19:25:10 +0200

    fish: ssh: force usage of sh to avoid conflicts

--- fish/.config/fish/functions/ssh.fish
+++ fish/.config/fish/functions/ssh.fish
@@ -1,8 +1,8 @@
 function ssh
-	command ssh $argv 'which fish >/dev/null 2>&1 && which git >/dev/null 2>&1 && which stow >/dev/null 2>&1 || exit 1 \
+	command ssh $argv 'sh -c \'which fish >/dev/null 2>&1 && which git >/dev/null 2>&1 && which stow >/dev/null 2>&1 || exit 1 \
 		&& [ -d "$HOME/.dotfiles" ] && { cd "$HOME/.dotfiles" && git pull && [ -L "$HOME/.config/fish" ] || { rm -rf "$HOME/.config/fish" && cd "$HOME/.dotfiles" && stow fish oh-my-fish && [ -d "$HOME/.local/share/omf" ] || fish -c oh-my-fish-init ; }; } \
 		|| { git clone https://github.com/Rohrschacht/dotfiles.git "$HOME/.dotfiles" && cd "$HOME/.dotfiles" && stow fish oh-my-fish vim \
-		 && fish -c oh-my-fish-init; }'
+		 && fish -c oh-my-fish-init; }\''
 
 	if not test $status -eq 0
 		echo ssh-bootstrap-config: Host does not have fish, git and stow installed or they are not in path
