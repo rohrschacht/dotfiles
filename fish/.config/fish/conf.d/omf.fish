2eb8d8cbf851626844700e0163c9338ff529f047
Tobias Petrich <tobias.petrich@campus.tu-berlin.de>
2021-09-06 21:33:14 +0200

    fish: only load oh-my-fish when shell is interactive

--- fish/.config/fish/conf.d/omf.fish
+++ fish/.config/fish/conf.d/omf.fish
@@ -4,4 +4,7 @@ set -q XDG_DATA_HOME
   or set -gx OMF_PATH "$HOME/.local/share/omf"
 
 # Load Oh My Fish configuration.
-source $OMF_PATH/init.fish
+# Only when the shell is interactive
+if status is-interactive
+	source $OMF_PATH/init.fish
+end
