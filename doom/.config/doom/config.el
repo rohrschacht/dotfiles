e555282b3e9dac571a8441a094f36b477b7995e3
Tobias Petrich <tobias.petrich@campus.tu-berlin.de>
2021-09-16 00:28:00 +0200

    doom: change back to DejaVu Sans Mono

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -20,7 +20,7 @@
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 14))
-(setq doom-font (font-spec :family "Inconsolata" :size 20))
+(setq doom-font (font-spec :family "DejaVu Sans Mono" :size 18))
 ;; (setq doom-font (font-spec :family "Fira Code" :size 14))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
