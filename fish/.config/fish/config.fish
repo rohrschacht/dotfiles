2e51e6ac1872639dfe9d7790aeeb8fa28651f4e7
Tobias Petrich <tobias.petrich@campus.tu-berlin.de>
2022-04-20 18:06:06 +0200

    fish: add flatpak XDG_DATA_DIRS discover

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -23,3 +23,11 @@ end
 if command -v starship >/dev/null 2>&1
 	starship init fish | source
 end
+
+if command -vq flatpak
+    set -ga fish_user_paths ~/.local/share/flatpak/exports/bin /var/lib/flatpak/exports/bin
+    set -gx --path XDG_DATA_DIRS /usr/local/share/ /usr/share/ ~/.local/share/flatpak/exports/share 
+    for install_dir in (flatpak --installations)
+        set -gxa XDG_DATA_DIRS $install_dir/exports/share
+    end
+end
