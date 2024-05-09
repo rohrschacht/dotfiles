49ce09929cf4d7dcdd30677a653058996960b1a5
Tobias Petrich <tobias.petrich@campus.tu-berlin.de>
2024-05-09 11:01:25 +0200

    update install scripts

--- init-openSUSE.zsh
+++ init-openSUSE.zsh
@@ -2,18 +2,19 @@
 
 XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 
-"""
-Install essential software for the setup:
-    - zsh (already installed for this script)
-    - tmux
-    - alacritty
-    - eza
-    - fd
-    - ripgrep
-    - rofi (wayland)
-"""
+# ================================================================================
+# Install essential software for the setup:
+#     - zsh (already installed for this script)
+#     - stow
+#     - tmux
+#     - alacritty
+#     - eza
+#     - fd
+#     - ripgrep
+#     - rofi (wayland)
+# ================================================================================
 
-sudo zypper install tmux alacritty eza fd ripgrep rofi-wayland
+sudo zypper install stow tmux alacritty eza fd ripgrep rofi-wayland
 
 stow alacritty
 stow ideavim
--- init.bash
+++ init.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-"""
-Install zsh and start setup for specific distro
-"""
+# ================================================================================
+# Install zsh and start setup for specific distro
+# ================================================================================
 
 if grep openSUSE /etc/os-release 2>&1; then
 	sudo zypper install zsh
